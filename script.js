45c2d38cc459a3397432902cd343fc7c8c1821c2
andreynho2006 <andreynho2006@gmail.com>
2017-05-10 07:42:07 +0300

    make a variable to add the values

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 var mybtn=document.getElementsByClassName("cbutton");
     var myOutput=document.getElementById("output");
+
+    var myCal="";
+
     console.log(mybtn);
     for(var i=0; i<mybtn.length; i++){
         mybtn[i].addEventListener("click",function(){
             var myValue=this.innerHTML;
-            myOutput.innerHTML=myValue;
-            console.log("clicked");
+            
+            myCal += myValue; 
+            myOutput.innerHTML = myCal;          
         });
     }
