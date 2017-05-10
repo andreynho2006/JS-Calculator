9657471884c7996125e0243b6eb2fcf290d7959f
andreynho2006 <andreynho2006@gmail.com>
2017-05-10 07:56:23 +0300

    make a new var myCom, set it to false and after we make an operation the rezult will be erase if we begin another op

--- script.js
+++ script.js
@@ -2,14 +2,21 @@ var mybtn=document.getElementsByClassName("cbutton");
     var myOutput=document.getElementById("output");
 
     var myCal="";
+    var myCom = false;
 
     console.log(mybtn);
     for(var i=0; i<mybtn.length; i++){
         mybtn[i].addEventListener("click",function(){
             var myValue=this.innerHTML;
             
+            if(myCom){
+                myCom=false;
+                myCal="";
+            }
+            
             if(myValue=="="){
                 myCal = eval(myCal);
+                myCom=true;
             }else if(myValue=="C"){
                 myCal = 0;
             }else{
