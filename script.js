1c50d37ea2a4d608b67f03a2b4c2257781eaa650
andreynho2006 <andreynho2006@gmail.com>
2017-05-10 08:04:44 +0300

    make a new variable to autoupdate the result after pressing the second operator of an operation

--- script.js
+++ script.js
@@ -3,24 +3,36 @@ var mybtn=document.getElementsByClassName("cbutton");
 
     var myCal="";
     var myCom = false;
+    var mySwitch = false; 
 
     console.log(mybtn);
     for(var i=0; i<mybtn.length; i++){
         mybtn[i].addEventListener("click",function(){
             var myValue=this.innerHTML;
             
-            if(myCom){
+            if(myCom || myCal=="0"){
                 myCom=false;
                 myCal="";
             }
+            
             
             if(myValue=="="){
                 myCal = eval(myCal);
                 myCom=true;
             }else if(myValue=="C"){
                 myCal = 0;
+                myCom=true;
             }else{
                 myCal += myValue; 
+            }
+            
+            if(mySwitch){
+                mySwitch = false;
+                myCal = eval(myCal);
+            }
+            
+            if(myValue=="+" || myValue=="-" || myValue=="*" || myValue=="/"){
+                mySwitch = true;
             }
              
             myOutput.innerHTML = myCal;          
